18930ec5cb9aee9f77868e7d2c96eebefa0bdc48
Lucas Spencer <lucas.spencer14@gmail.com>
2022-05-31 22:36:27 -0600

    4more

--- jobs/app.py
+++ jobs/app.py
@@ -9,6 +9,7 @@ def open_connection():
     connection = getattr(g, '_connection', None)
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = squlite3.Row
     return connection
 
 
